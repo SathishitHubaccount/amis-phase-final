eed24c504b314d058b012d392996fe25b3db5e27
AMIS Project <amis@project.local>
2026-04-02 00:37:23 +0530

    Remove all documentation files, keep only code

--- START_BACKEND.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-title AMIS Backend Server
-cd /d "%~dp0backend"
-echo Starting AMIS Backend on http://127.0.0.1:8080 ...
-python -c "import uvicorn; uvicorn.run('main:app', host='127.0.0.1', port=8080, reload=False)"
-pause
